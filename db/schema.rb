169d0fa63ca7ea71b521fa3b6b0505ff48a58219
foklepoint <live.mellon@gmail.com>
2016-05-06 23:59:34 -0700

    rubocop schema fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506071259) do
-
+ActiveRecord::Schema.define(version: 20_160_506_071_259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160506071259) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -33,5 +32,4 @@ ActiveRecord::Schema.define(version: 20160506071259) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
